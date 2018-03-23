230dedf6e54e60fe5b8bddb07f9a9c0ef99531b2
Tim Work Laptop <tmstani23@gmail.com>
2018-03-23 15:36:26 -0500

    Update 4
    Changed code array to data instead of matrix for readability.  Added static versions of the main matrix functions.  Added map function that maps a function to each value in the data array.  Added weights to the input to hidden connections and the hidden to output connections.  Added sigmoud function to calculate exponents.  Added nn calls to the sketch to test.

--- matrix.js
+++ matrix.js
@@ -1,32 +1,51 @@
 //let m = new Matrix(3,2)
 //note use console.table(m.matrix) to view the matrix as a table
-console.log("working");
+
 class Matrix {
     constructor(rows, cols) {
         this.rows = rows;
         this.cols = cols;
-        this.matrix = [];
+        this.data = [];
         //loop through each row
         for (let i = 0; i < this.rows; i++) {
             //each iteration element is an array 
             //each array is a column
-            this.matrix[i] = [];
+            this.data[i] = [];
             
             //loop until value of cols is reached
             for (let j = 0; j < this.cols; j++) {
                 //fill the 2d array with 0 values
-                this.matrix[i][j] = 0;
+                this.data[i][j] = 0;
             }
         }
+    }
+
+
+    static fromArray(arr) {
+        let m = new Matrix(arr.length, 1);
+        for(let i=0; i < arr.length; i++) {
+            m.data[i][0] = arr[i];
+        }
+        
+        return m;
+    }
+
+    toArray() {
+        let arr = [];
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                arr.push(this.data[i][j]); 
+            }
+        }
+        return arr;
     }
 
     randomize(n) {
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
-                //fill the matrix with random values between 0 and 10
-                //math.floor rounds numbers down to the nearest whole
-                //math.random returns a number between 0 and 1
-                this.matrix[i][j] = Math.floor(Math.random() * 10);
+                
+                //randomize returns a number between -1 and 1:
+                this.data[i][j] = Math.random() * 2 - 1;
             }
         }
     }
@@ -40,7 +59,7 @@ class Matrix {
                 for (let j = 0; j < this.cols; j++) {
                     //add all the values from the first matrix 
                     //by the values of the second matrix
-                    this.matrix[i][j] += n.matrix[i][j];
+                    this.data[i][j] += n.data[i][j];
                 }
             }    
         } 
@@ -49,50 +68,69 @@ class Matrix {
         else {
             for (let i = 0; i < this.rows; i++) {
                 for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] += n;
+                this.data[i][j] += n;
                 }
             }
         }
     }
 
-    transpose() {
+    static transpose(matrix) {
         let result = new Matrix(this.cols, this.rows);
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
-            result.matrix[j][i] = this.matrix[i][j];
+            result.data[j][i] = this.data[i][j];
             }
         }
         return result;
     }
 
-    multiply(n) {
+    static multiply(a, b) {
+        //Matrix product
+        if (a.cols !== b.rows) {
+            console.log("Columns of A must match Rows of B");
+            return undefined;
+        }
+            
+        let result = new Matrix(a.rows, b.cols);
+        for (let i = 0; i < result.rows; i++) {
+            for (let j = 0; j < result.cols; j++) {
+                let sum = 0;
+                for(let k = 0; k < a.cols; k++) {
+                    sum += a.data[i][k] * b.data[k][j];
+                }
+                result.data[i][j] = sum;
+            }
+        }
+        return result;
+    }
         
+    multiply(n) {
         if (n instanceof Matrix) {
-            if(this.cols !== n.rows) {
-                console.log("Columns of A must match Rows of B");
-                return undefined;
+          // hadamard product
+          for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+              this.data[i][j] *= n.data[i][j];
             }
-            let a = this;
-            let b = n;
-            let result = new Matrix(a.rows, b.cols);
-            for (let i = 0; i < result.rows; i++) {
-                for (let j = 0; j < result.cols; j++) {
-                    let sum = 0;
-                    for(let k = 0; k < a.cols; k++) {
-                        sum += a.matrix[i][k] * b.matrix[k][j];
-                    }
-                    result.matrix[i][j] = sum;
-                }
-            }
-            return result;
+          }
         } else {
-            //Take a single value "n" and multiply every 
-            //element of the matrix by that value    
-            for (let i = 0; i < this.rows; i++) {
-                for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] *= n;
-                }
+          // Scalar product
+          for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+              this.data[i][j] *= n;
+            }
+          }
+        }
+    }
+    
+    map(func) {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                let val = this.data[i][j];
+                this.data[i][j] = func(val);
             }
         }
     }
+
+
 }
+
--- sketch.js
+++ sketch.js
@@ -1,16 +1,10 @@
 function setup() {
-    let a = new Matrix(2, 3);
-    a.randomize();
-    let b = a.transpose();
-    console.table(a.matrix);
-    console.table(b.matrix);
-    // let b = new Matrix(3, 2);
-    // b.randomize();
-    // console.table(a.matrix);
-    // console.table(b.matrix);
+    
+    let nn = new NeuralNetwork(2,2,1);
+    let input = [1, 0];
+    let output = nn.feedforward(input);
 
-    // let c = a.multiply(b);
-    // console.table(c.matrix);
+    console.log(output);
     
 }
 
