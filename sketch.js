cb57b8faad45475f37cccf572d94ddd4508bd89c
Tim Work Laptop <tmstani23@gmail.com>
2018-03-28 15:44:29 -0500

    Update 7
    Added training data to the sketch.  Added loop that trains the data 1000 times as the sketch is drawing.  Added a p5 canvas to display the data while it's training.  Added rectangles at each datapoint based on a resolution representing its current data value.

--- sketch.js
+++ sketch.js
@@ -1,40 +1,54 @@
+
+
+let nn;
+
 let training_data = [
     {
+        inputs: [0,0],
+        outputs: [0]
+    },
+    {
         inputs: [0,1],
-        targets: [1]
+        outputs: [1]
     },
     {
         inputs: [1,0],
-        targets: [1]
-    },
-    {
-        inputs: [0,0],
-        targets: [0]
+        outputs: [1]
     },
     {
         inputs: [1,1],
-        targets: [0]
+        outputs: [0]
     }
 ]
 
-
-
 function setup() {
     
-    let nn = new NeuralNetwork(2,2,1);
-    
-    for (let i = 0; i < 50000; i++) {
-        let data = random(training_data);
-        nn.train(data.inputs, data.targets);
-        
-    }
-   
-    console.log(nn.feedforward([1,0]));
-    console.log(nn.feedforward([0,1]));
-    console.log(nn.feedforward([1,1]));
-    console.log(nn.feedforward([0,0]));
+    createCanvas(400,400);
+    nn = new NeuralNetwork(2,2,1);
+
+
 }
 
 function draw() {
+    background(0);
+
+    for (let i = 0; i < 1000; i++) {
+        //Note random is a p5 method
+        let data = random(training_data);
+        nn.train(data.inputs, data.outputs);
+    }
+    
+    let resolution = 10;
+    let cols = width / resolution;
+    let rows = height / resolution;
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            fill(random(255));
+            rect(i * resolution, j * resolution, resolution, resolution);
+        }
+    }
+    
+    console.log(nn.feedforward([1,0]));
 
 }
